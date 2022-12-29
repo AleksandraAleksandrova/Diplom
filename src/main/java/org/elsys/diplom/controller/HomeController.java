b8cd17b41bfb154a8928730b949e75ea3f2aa975
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2022-12-29 22:32:13 +0200

    add categories in db with controller, works fine

--- src/main/java/org/elsys/diplom/controller/HomeController.java
+++ src/main/java/org/elsys/diplom/controller/HomeController.java
@@ -1,10 +1,18 @@
 package org.elsys.diplom.controller;
 
+import org.elsys.diplom.entity.Category;
+import org.elsys.diplom.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
+    @Autowired
+    CategoryRepository categoryRepository;
+
     @GetMapping("/welcome")
     public String getWelcomePage(){
         return "welcome";
@@ -17,5 +25,16 @@ public class HomeController {
     @GetMapping("/register")
     public String getRegisterPage(){
         return "register";
+    }
+
+    @GetMapping("/addCategory")
+    public String getAddCategoryPage(){
+        return "addCategory";
+    }
+
+    @PostMapping("/addCategory")
+    public String postAddCategoryPage(@ModelAttribute Category category){
+        categoryRepository.save(category);
+        return "redirect:/welcome";
     }
 }
--- src/main/java/org/elsys/diplom/entity/Expense.java
+++ src/main/java/org/elsys/diplom/entity/Expense.java
@@ -30,4 +30,54 @@ public class Expense {
     @JoinColumn(name = "category_id", referencedColumnName = "id")
     private Category category;
 
+    public Expense() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
