004e8871ab3684a67f30379cdf77b1ad4dddd399
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-01-04 23:07:05 +0200

    user-related dto objects

---
+++ src/main/java/org/elsys/diplom/dto/UserDTO.java
@@ -0,0 +1,13 @@
+package org.elsys.diplom.dto;
+
+public class UserDTO {
+    private String username;
+    private String email;
+
+    public UserDTO(){}
+
+    public UserDTO(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+}
