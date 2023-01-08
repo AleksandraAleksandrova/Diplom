b7f29d8fe29046d608ac1ce33b7670133c1c5fb6
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-01-08 12:34:14 +0200

    configure spring security 6

--- src/main/java/org/elsys/diplom/security/SecurityConfiguration.java
+++ src/main/java/org/elsys/diplom/security/SecurityConfiguration.java
@@ -4,30 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/home").authenticated()
-                .antMatchers("/statistics").authenticated()
-                .antMatchers("/help").authenticated()
-                .antMatchers("/welcome").permitAll()
-                .antMatchers("/home").authenticated()
-                .anyRequest().permitAll()
-                .and()
-                .formLogin().loginPage("/login").defaultSuccessUrl("/home").permitAll()
-                .and()
-                .logout().logoutSuccessUrl("/welcome").permitAll();
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/login", "/register", "/welcome").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin(form -> {
+                    form.loginPage("/login");
+                    form.defaultSuccessUrl("/home", true);
+                })
+                .logout(logout -> {
+                    logout.logoutUrl("/logout");
+                    logout.logoutSuccessUrl("/welcome");
+                })
+                .build();
     }
 }
